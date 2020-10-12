59fcff3121dba4cb9bd501e31bf7019e7dabae2b
LuoJingYan <921402781@qq.com>
2020-10-12 22:46:39 +0800

    修改Terminal实现

--- src/main/java/com/bolo/downloader/Terminal.java
+++ src/main/java/com/bolo/downloader/Terminal.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Terminal {
@@ -15,30 +14,32 @@ public class Terminal {
     private static final String flag = "[download] 100% ";
 
     public static boolean execYoutubeDL(String url, String filePath) {
+        return exec("youtube-dl " + url, flag);
+    }
+
+    private static boolean exec(String command, String flag) {
+        boolean result = false;
         Process process = null;
         BufferedReader commandLineReader = null;
+        BufferedReader errorReader = null;
         try {
-            String command = String.format("youtube-dl --exec \"mv {} %s{}\"  %s", filePath, url);
             log.info(IDIOMATIC, command);
             process = Runtime.getRuntime().exec(command);
             commandLineReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            if (commandLineReader.ready()) {
-                String export;
-                while ((export = commandLineReader.readLine()) != null) {
-                    log.info(IDIOMATIC, export);
-                    if (export.contains(flag)) {
-                        log.info("已探测到命令行进程完成标志，等待进程结束...");
-                        if (process.waitFor(2, TimeUnit.SECONDS)) {
-                            log.info("命令行进程已结束.");
-                        } else {
-                            log.info("命令行进程执行超时，强制退出.");
-                        }
-                        break;
-                    }
+            errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String export;
+            while ((export = commandLineReader.readLine()) != null) {
+                log.info(IDIOMATIC, export);
+                if (export.contains(flag)) {
+                    log.info("已探测到命令行进程成功标志");
+                    result = true;
                 }
             }
-            return true;
-        } catch (IOException | InterruptedException e) {
+            while ((export = errorReader.readLine()) != null) {
+                log.error(IDIOMATIC, export);
+            }
+            log.info("命令行执行完毕.");
+        } catch (IOException e) {
             log.error("命令行执行异常！异常信息：" + e.getMessage(), e);
         } finally {
             if (null != commandLineReader) {
@@ -48,10 +49,17 @@ public class Terminal {
                     log.error("命令行的输入流资源释放发生异常，异常信息：{}" + e.getMessage());
                 }
             }
+            if (null != errorReader) {
+                try {
+                    errorReader.close();
+                } catch (IOException e) {
+                    log.error("命令行的异常输入流资源释放发生异常，异常信息：{}" + e.getMessage());
+                }
+            }
             if (null != process && process.isAlive()) {
                 process.destroyForcibly();
             }
         }
-        return false;
+        return result;
     }
 }
