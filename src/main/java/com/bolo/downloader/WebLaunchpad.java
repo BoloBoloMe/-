111e318ae12366dcc0580eea07c4ca3ab9df2d90
bolo <921402781@qq.com>
2020-10-14 14:27:40 +0800

    实现任务列表页面

--- src/main/java/com/bolo/downloader/WebLaunchpad.java
+++ src/main/java/com/bolo/downloader/WebLaunchpad.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class WebLaunchpad {
@@ -17,7 +17,7 @@ public class WebLaunchpad {
 
     @PostMapping("task/add")
     public ResponseEntity<String> addTask(@RequestParam("url") String url) {
-        return ResponseEntity.status(HttpStatus.OK).body(downloader.addTask(url) == 1 ? "添加成功" : "任务已在列表中！");
+        return ResponseEntity.status(HttpStatus.OK).body(downloader.addTask(new String(Base64.getDecoder().decode(url))) == 1 ? "添加成功" : "任务已在列表中！");
     }
 
     @RequestMapping("task/start")
@@ -32,7 +32,20 @@ public class WebLaunchpad {
     }
 
     @RequestMapping("task/list")
-    public ResponseEntity<Map<String, String>> listTask() {
-        return ResponseEntity.status(HttpStatus.OK).body(downloader.list());
+    public ResponseEntity<Map<String, Object>> listTask() {
+        Map<String, String> list = downloader.list();
+        // {"code":0,"msg":"",data:[{}]}
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", "0");
+        result.put("msg", "成功");
+        List<Map<String, String>> data = new ArrayList<>();
+        for (String url : list.keySet()) {
+            Map<String, String> item = new HashMap<>();
+            item.put("url", url);
+            item.put("status", list.get(url));
+            data.add(item);
+        }
+        result.put("data", data);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
