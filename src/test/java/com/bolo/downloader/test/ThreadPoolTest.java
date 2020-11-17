998b430f4e517d0b762c85205e4922d71ca1175d
LuoJingYan <921402781@qq.com>
2020-11-18 03:36:52 +0800

    下载线程扩展到5个

--- src/test/java/com/bolo/downloader/test/ThreadPoolTest.java
+++ src/test/java/com/bolo/downloader/test/ThreadPoolTest.java
@@ -1,10 +1,17 @@
 package com.bolo.downloader.test;
 
+import org.junit.Test;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class ThreadPoolTest {
-    public static void main(String[] args) {
+    @Test
+    public void testFixedThreadPool() {
         ExecutorService threadPool = Executors.newFixedThreadPool(1);
 
         System.out.println("第一次调用线程池");
@@ -30,5 +37,13 @@ public class ThreadPoolTest {
             System.out.println("第三次调用线程池-线程id:" + Thread.currentThread().getId());
         });
         System.out.println("主线程结束");
+        while (true) ;
+    }
+
+    @Test
+    public void testScheduledThreadPool() {
+        ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(1);
+        threadPool.scheduleAtFixedRate(() -> System.out.println("执行ScheduledThreadTask"), 5,10, SECONDS);
+        while (true) ;
     }
 }
