260423ac14c7656d594c94a845257470d9735b78
luojingyan <921402781@qq.com>
2021-09-18 01:53:36 +0800

    调整代码

--- ResourcePool/src/main/java/com/bolo/downloader/respool/nio/http/invoke/BaseMethodInvoker.java
+++ ResourcePool/src/main/java/com/bolo/downloader/respool/nio/http/invoke/BaseMethodInvoker.java
@@ -5,11 +5,14 @@ import com.bolo.downloader.respool.log.MyLogger;
 import com.bolo.downloader.respool.nio.http.HttpDistributeHandler;
 import com.bolo.downloader.respool.nio.http.scan.MethodMapper;
 import com.bolo.downloader.respool.nio.http.scan.MethodMapperContainer;
-import com.sun.istack.internal.Nullable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.handler.codec.http.multipart.MemoryAttribute;
 
 import java.lang.reflect.Method;
 import java.util.*;
@@ -17,8 +20,9 @@ import java.util.*;
 /**
  * 方法调用的共用处理过程
  */
-abstract public class BaseMethodInvoker implements MethodInvoker {
+public class BaseMethodInvoker implements MethodInvoker {
     private static final MyLogger log = LoggerFactory.getLogger(HttpDistributeHandler.class);
+    private static final ThreadLocal<Map<String, List<String>>> parametersHolder = new ThreadLocal<>();
 
     @Override
     public ResponseEntity invoke(ChannelHandlerContext ctx, FullHttpRequest request) {
@@ -28,7 +32,7 @@ abstract public class BaseMethodInvoker implements MethodInvoker {
         final String uri = request.uri();
         final HttpMethod requestMethod = request.method();
         if (Objects.isNull(uri) || Objects.isNull(requestMethod)) {
-            return new ResponseEntity( HttpResponseStatus.BAD_REQUEST, "invalid request");
+            return new ResponseEntity(HttpResponseStatus.BAD_REQUEST, "invalid request");
         }
         final MethodMapper methodMapper = MethodMapperContainer.get(uri);
         if (Objects.isNull(methodMapper)) {
@@ -41,22 +45,60 @@ abstract public class BaseMethodInvoker implements MethodInvoker {
             return new ResponseEntity(HttpResponseStatus.METHOD_NOT_ALLOWED, "allowed method " + methodMapper.getAllowedMethods());
         }
         try {
-            final Object[] parameters = alignParameters(getParameters(ctx, request), method);
-            Object result = method.invoke(instance, parameters);
+            Map<String, List<String>> parameterMap = getParameters(ctx, request, uri, requestMethod);
+            Object[] parameterList = alignParameters(ctx, request, parameterMap, method);
+            parametersHolder.set(parameterMap);
+            Object result = method.invoke(instance, parameterList);
             return interpretResponseEntity(result);
         } catch (Exception e) {
             log.error("http request handler invoke failed. method=" + method.getName() + ",parameters={}." + Collections.emptyList(), e);
+        } finally {
+            parametersHolder.remove();
         }
         return new ResponseEntity(HttpResponseStatus.INTERNAL_SERVER_ERROR);
     }
 
-    abstract Map<String, List<String>> getParameters(ChannelHandlerContext ctx, FullHttpRequest request);
+    private Map<String, List<String>> getParameters(ChannelHandlerContext ctx, FullHttpRequest request, String uri, HttpMethod requestMethod) {
+        Map<String, List<String>> params = new HashMap<>();
+        if (HttpMethod.GET.equals(requestMethod)) {
+            int beginIndex = uri.indexOf('?');
+            if (beginIndex < 0) {
+                return params;
+            }
+            String[] entryList = uri.substring(beginIndex + 1).split("&");
+            for (String entry : entryList) {
+                String[] entryArr = entry.split("=");
+                if (entryArr.length != 2) continue;
+                List<String> list = params.computeIfAbsent(entryArr[0], k -> new LinkedList<>());
+                list.add(entryArr[1]);
+            }
+            return params;
+        } else if (HttpMethod.POST.equals(requestMethod)) {
+            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(new DefaultHttpDataFactory(false), request);
+            List<InterfaceHttpData> parmList = decoder.getBodyHttpDatas();
+            for (InterfaceHttpData parm : parmList) {
+                if (parm.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
+                    MemoryAttribute data = (MemoryAttribute) parm;
+                    List<String> values;
+                    if (null == (values = params.get(data.getName()))) {
+                        values = new ArrayList<>();
+                        values.add(data.getValue());
+                        params.put(data.getName(), values);
+                    } else {
+                        values.add(data.getValue());
+                    }
+                }
+            }
+        }
+        return params;
+    }
 
-    @Nullable
-    abstract ResponseEntity interpretResponseEntity(@Nullable Object result);
+    ResponseEntity interpretResponseEntity(Object result) {
+        return new ResponseEntity(HttpResponseStatus.NOT_IMPLEMENTED);
+    }
 
 
-    private Object[] alignParameters(Map<String, List<String>> parameters, Method method) {
+    private Object[] alignParameters(ChannelHandlerContext ctx, FullHttpRequest request, Map<String, List<String>> parameters, Method method) {
         return new Object[]{};
     }
 }
